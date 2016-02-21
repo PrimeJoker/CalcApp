0dbf1524fdda44cab6c15516d1f465947f0d34d2
PrimeJoker <jamesomambia@gmail.com>
2016-02-21 01:19:30 +0000

    Added files via upload

---
+++ Calc1/app/build/generated/source/buildConfig/armFreeapp/debug/com/brsyntex/james/calc1/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.brsyntex.james.calc1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.brsyntex.james.calc1";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "armFreeapp";
+  public static final int VERSION_CODE = 10100;
+  public static final String VERSION_NAME = "0.1.1-SNAPSHOT";
+  public static final String FLAVOR_abi = "arm";
+  public static final String FLAVOR_version = "freeapp";
+}
---
+++ Calc1/app/build/generated/source/buildConfig/armFreeapp/release/com/brsyntex/james/calc1/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.brsyntex.james.calc1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.brsyntex.james.calc1";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "armFreeapp";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "0.1.0-SNAPSHOT";
+  public static final String FLAVOR_abi = "arm";
+  public static final String FLAVOR_version = "freeapp";
+}
